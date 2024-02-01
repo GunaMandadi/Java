d743266c8d1924fe4606bf0edd2395429a827a53
Guna Mandadi <gunashekar166@gmail.com>
2024-01-31 23:15:11 -0600

    polymorphism

---
+++ src/main/java/oops/Child.java
@@ -0,0 +1,7 @@
+package oops;
+
+public class Child {
+    public void method() {
+        System.out.println("Child method called");
+    }
+}
---
+++ src/main/java/oops/Polymorphism.java
@@ -0,0 +1,11 @@
+package oops;
+
+public class Polymorphism {
+    public static void main(String[] args) {
+        Parent obj1 = new Parent();
+        obj1.method();
+
+        Parent obj2 = new Parent();
+        obj2.method();
+    }
+}
